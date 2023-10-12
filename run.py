5e1fbfc4eb76989e4ea82d3320d73e1774bd88ef
Pikachubolk <58044069+Pikachubolk@users.noreply.github.com>
2023-10-12 19:10:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,10 +4,12 @@ import asyncio
 from datetime import datetime, timedelta
 from discord import Embed
 import pytz
+import pymongo
 
 # Your credentials
 DISCORD_TOKEN = 'TOKEN'
 NS_API_KEY = 'NSPRIMARYKEY'
+MONGO_URI = 'MONGODB_CONNECTION_URL'
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -20,35 +22,49 @@ station_names = {
     'STD': 'Sittard',
     'EHV': 'Eindhoven',
     'RM': 'Roermond',
+    'BDE': 'Bunde',
+    'MES': 'Meerssen',
+    'LG': 'Landgraaf',
+    'EDN': 'Eijsden'
 }
+
+client.db = pymongo.MongoClient(MONGO_URI).db
 
 async def fetch(url, session, headers):
     async with session.get(url, headers=headers) as response:
         return await response.json()
 
 async def check_delays():
-    # List of station codes to check
-    stations = ['MT', 'MTR', 'STD', 'EHV', 'RM']  
+    stations = ['MT', 'MTR', 'STD', 'EHV', 'RM', 'BDE', 'MES', 'LG', 'EDN']  
 
     async with aiohttp.ClientSession() as session:
         while True:
+            next_check_time = datetime.now(pytz.timezone('Europe/Amsterdam')) + timedelta(minutes=5)  # Default to 5 minutes
             for station_code in stations:
                 headers = {
                     'Ocp-Apim-Subscription-Key': NS_API_KEY,
                 }
 
-                # Check for disruptions
                 disruption_data = await fetch(f'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions/station/{station_code}', session, headers)
-
-                # Check for departures
                 departures_data = await fetch(f'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?station={station_code}', session, headers)
 
-                delay_embeds = []
-
-                # Get the current time in the Netherlands (Central European Time)
                 now = datetime.now(pytz.timezone('Europe/Amsterdam'))
 
+                all_departures = {}
                 for departure in departures_data.get('payload', {}).get('departures', []):
+                    name = departure.get('name', 'Onbekende naam')
+                    if name in all_departures:
+                        existing_time_str = all_departures[name].get('actualDateTime', '')
+                        existing_time = datetime.strptime(existing_time_str, "%Y-%m-%dT%H:%M:%S%z")
+                        new_time_str = departure.get('actualDateTime', '')
+                        new_time = datetime.strptime(new_time_str, "%Y-%m-%dT%H:%M:%S%z")
+                        if new_time < existing_time:
+                            all_departures[name] = departure
+                    else:
+                        all_departures[name] = departure
+
+                delay_embeds = {}
+                for departure_name, departure in all_departures.items():
                     planned_time_str = departure.get('plannedDateTime', '')
                     actual_time_str = departure.get('actualDateTime', '')
                     planned_time = datetime.strptime(planned_time_str, "%Y-%m-%dT%H:%M:%S%z")
@@ -63,20 +79,47 @@ async def check_delays():
                         actual_unix_timestamp = int((actual_time - datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds())
                         status_text = "Geannuleerd" if is_cancelled else ("Vertraagd" if is_delayed else "Op tijd")
                         station_of_origin = station_names.get(station_code, 'Onbekend station')
-                        title = f"Naar: {departure.get('direction', 'Onbekende richting')} - {departure.get('name', 'Onbekende naam')}"
+                        title = f"Naar: {departure.get('direction', 'Onbekende richting')} - {departure_name}"
                         description = f"Geplande Tijd: <t:{planned_unix_timestamp}:T>\nActuele Tijd: <t:{actual_unix_timestamp}:T>\nStatus: {status_text}\nStation van herkomst: {station_of_origin}"
                         embed_color = 0xff0000 if is_cancelled or is_delayed else 0x00ff00
                         embed = Embed(title=title, description=description, color=embed_color)
-                        delay_embeds.append(embed)
+                        delay_embeds[departure_name] = embed
 
-                # If there are any delays or disruptions, send an embed to the specified Discord channel
-                if delay_embeds:
-                    channel = client.get_channel(1150686728403828807)  # Replace with your actual channel ID
-                    if channel:
-                        for embed in delay_embeds:
-                            await channel.send(embed=embed)
+                    # Calculate the time at which this message should be deleted
+                    deletion_time = actual_time + timedelta(minutes=2)
+                    # Update next_check_time if this deletion_time is sooner
+                    next_check_time = min(next_check_time, deletion_time)
 
-            await asyncio.sleep(60 * 10)  # Check every 10 minutes
+                channel = client.get_channel(1150686728403828807)  # Replace with your actual channel ID
+                if channel:
+                    current_entries = client.db.entries.find({"station_code": station_code})
+                    for entry in current_entries:
+                        message_id = entry['message_id']
+                        departure_id = entry['departure_id']
+                        if departure_id not in delay_embeds:
+                            message = await channel.fetch_message(message_id)
+                            await message.delete()
+                            client.db.entries.delete_one({"_id": entry['_id']})
+                        else:
+                            message = await channel.fetch_message(message_id)
+                            await message.edit(embed=delay_embeds[departure_id])
+                            del delay_embeds[departure_id]
+
+                    for departure_id, embed in delay_embeds.items():
+                        message = await channel.send(embed=embed)
+                        client.db.entries.insert_one({
+                            "station_code": station_code,
+                            "departure_id": departure_id,
+                            "message_id": message.id,
+                            "actualDateTime": departures_data.get('actualDateTime', '')
+                        })
+
+            # Calculate the sleep interval as the difference between now and next_check_time
+            sleep_interval = (next_check_time - datetime.now(pytz.timezone('Europe/Amsterdam'))).total_seconds()
+            # Ensure sleep_interval is at least 1 second to prevent busy-waiting
+            sleep_interval = max(sleep_interval, 1)
+            
+            await asyncio.sleep(sleep_interval)
 
 @client.event
 async def on_ready():
@@ -84,4 +127,3 @@ async def on_ready():
     client.loop.create_task(check_delays())
 
 client.run(DISCORD_TOKEN)
-
